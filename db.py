597a466e5e3a37cb199f5a12a0c6198fa01aa41e
Dakoro <david.korobetski@gmail.com>
2023-02-07 16:58:44 +0100

    update db.py

--- db.py
+++ db.py
@@ -1,32 +1,31 @@
 from sqlalchemy import create_engine
 import pandas as pd
-'''
-with open("config.yml") as file:
+import yaml
+with open('config.yaml', 'r') as file:
+    config = yaml.safe_load(file)
     
-host="greta-p2-g2.westeurope.cloudapp.azure.com"
+cfg=config['PG']
+print(cfg)
 
-pgSQLengine = create_engine(
-"postgresql+psycopg2://%s:%s@%s/%s" %
-("postgres", "greta2023", host, 'test'))
+url = "{driver}://{user}:{password}@{host}/{database}".format(**cfg)
+print('URL', url)
 
-df = pd.read_csv('https://datasets.imdbws.com/name.basics.tsv.gz', compression='gzip', sep='\t', nrows=10)
-df.to_sql('test_names', pgSQLengine)
-'''
+engine = create_engine(url)
 
+# load with pandas 
 names = pd.read_csv('https://datasets.imdbws.com/name.basics.tsv.gz', compression='gzip', sep='\t', nrows=1000)
 title = pd.read_csv('https://datasets.imdbws.com/title.akas.tsv.gz', compression='gzip', sep='\t', nrows=1000)
 basics = pd.read_csv('https://datasets.imdbws.com/title.basics.tsv.gz', compression='gzip', sep='\t', nrows=1000)
 crew = pd.read_csv('https://datasets.imdbws.com/title.crew.tsv.gz', compression='gzip', sep='\t', nrows=1000)
-episode = pd.read_csv('https://datasets.imdbws.com/title.episode.tsv.gz', compression='gzip', sep='\t', nrows=1000),
+episode = pd.read_csv('https://datasets.imdbws.com/title.episode.tsv.gz', compression='gzip', sep='\t', nrows=1000)
 principals = pd.read_csv('https://datasets.imdbws.com/title.principals.tsv.gz', compression='gzip', sep='\t', nrows=1000)
 ratings = pd.read_csv('https://datasets.imdbws.com/title.ratings.tsv.gz', compression='gzip', sep='\t', nrows=1000)
 
-names.to_sql('test_names', pgSQLengine, if_exists='replace')
-title.to_sql('test_names', pgSQLengine, if_exists='replace')
-basics.to_sql('test_names', pgSQLengine, if_exists='replace')
-crew.to_sql('test_names', pgSQLengine, if_exists='replace')
-episode.to_sql('test_names', pgSQLengine, if_exists='replace')
-principals.to_sql('test_names', pgSQLengine, if_exists='replace')
-ratings.to_sql('test_names', pgSQLengine, if_exists='replace')
-                            
-
+# sent data to database
+names.to_sql('names', engine, if_exists='replace')
+title.to_sql('title_akas', engine, if_exists='replace')
+basics.to_sql('title_basics', engine, if_exists='replace')
+crew.to_sql('title_crew', engine, if_exists='replace')
+principals.to_sql('title_principals', engine, if_exists='replace')
+ratings.to_sql('title_ratings', engine, if_exists='replace')
+episode.to_sql('title_episode', engine, if_exists='replace')
